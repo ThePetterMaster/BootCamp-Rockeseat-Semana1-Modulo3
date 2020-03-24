ede1548f035684404bce926a76d288833a926ec9
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-03-24 14:27:07 -0300

    Add files via upload

---
+++ Model e loaders de usuário/src/routes.js
@@ -0,0 +1,16 @@
+import {Router}from'express';
+
+import User from './app/models/User';
+
+const routes=new Router();
+
+routes.get('/',async(req,res)=>{
+     const user= await User.create({
+        name:'Pedro Alves',
+        email:'pedroneeto@hotmail.com',
+        password_hash:'123456',
+    });
+    return res.json(user);
+});
+
+export default routes;
