e409db2b87f9d363486bde5cee394bb4b2dbdfea
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-03-26 16:33:53 -0300

    Add files via upload

---
+++ Cadastro de Usuários/src/routes.js
@@ -0,0 +1,12 @@
+import {Router}from'express';
+
+import User from './app/models/User';
+
+import UserController from './app/controllers/UserController'
+
+const routes=new Router();
+
+routes.post('/',UserController.store);
+
+
+export default routes;
