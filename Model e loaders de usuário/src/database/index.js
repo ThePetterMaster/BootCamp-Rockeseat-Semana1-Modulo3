57394b88c526ebde47aecb8116c807cfd6568ea8
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-03-27 10:41:11 -0300

    Update index.js

--- Model e loaders de usuário/src/database/index.js
+++ Model e loaders de usuário/src/database/index.js
@@ -2,6 +2,8 @@ import Sequelize, { Model } from 'sequelize';
 import User from '../app/models/User';
 import databaseConfig from '../config/database';
 
+//Um vetor com todos os models criados
+//No caso só foi criado um model
 const models=[User];
 
 class Database{
@@ -10,6 +12,7 @@ class Database{
     }
     init(){
         this.connection=new Sequelize(databaseConfig);
+        //Para todos os models do vetor é executado a função init
         models.map(model=>model.init(this.connection));
     }
 }
