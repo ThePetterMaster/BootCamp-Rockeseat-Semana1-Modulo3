dc91eb0034428ee3576c8d5695c407df69663eea
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-03-27 10:56:50 -0300

    Update User.js

--- Gerando Hash da senha/src/app/models/User.js
+++ Gerando Hash da senha/src/app/models/User.js
@@ -6,6 +6,7 @@ class User extends Model{
         super.init({
             name:Sequelize.STRING,
             email:Sequelize.STRING,
+            //Esse campo não irá existir no banco de dados
             password:Sequelize.VIRTUAL,
             password_hash:Sequelize.STRING,
             provider:Sequelize.BOOLEAN,
@@ -14,7 +15,9 @@ class User extends Model{
             sequelize,
         }
         );
-
+        //Hooks são códigos que executam baseados em ações no código
+        //Nesse caso irá executar antes de ser salvado
+        //Esse 8 é o nível da criptografia
         this.addHook('beforeSave',async (user)=>{
             if(user.password){
                 user.password_hash=await bcrypt.hash(user.password,8)
