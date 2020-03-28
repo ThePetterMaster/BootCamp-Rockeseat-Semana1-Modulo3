c0639ff2f73998ee612157b8be9ea31d442e09c4
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-03-28 19:30:38 -0300

    Update database.js

--- Configurando Sequelize e Estrutura/src/config/database.js
+++ Configurando Sequelize e Estrutura/src/config/database.js
@@ -2,7 +2,7 @@ module.exports={
     dialect:'postgres',
     host:'localhost',
     username:'postgres',
-    passworld:'docker',
+    password:'docker',
     database:'gobarber',
     define:{
         timestamps:true,
